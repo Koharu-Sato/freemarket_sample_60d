a044d2afeedb76596716281bf01dc64ce2ef16f1
Koharu-Sato <54939418+Koharu-Sato@users.noreply.github.com>
2019-10-28 20:30:18 +0900

    push

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "freemarket_sample_60d"
-set :repo_url, "git@example.com:Koharu-Sato/freemarket_sample_60d.git"
+set :repo_url, "git@github.com:Koharu-Sato/freemarket_sample_60d.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
