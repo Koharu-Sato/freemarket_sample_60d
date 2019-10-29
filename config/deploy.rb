6417bfcb41df06ef6c94bfc3386f531ff3ebfec0
Koharu-Sato <54939418+Koharu-Sato@users.noreply.github.com>
2019-10-29 12:16:44 +0900

    comment out search.html.haml

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,10 +22,10 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  rbenv_root:
+  path:
+  AWS_ACCESS_KEY_ID:
+  AWS_SECRET_ACCESS_KEY:
 }
 
 set :linked_files, %w{ config/master.key }
