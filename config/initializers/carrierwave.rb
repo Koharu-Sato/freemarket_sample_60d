3da1504fea974aeece611580cb4083ade331b260
Koharu-Sato <54939418+Koharu-Sato@users.noreply.github.com>
2019-10-29 13:18:41 +0900

    edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,15 +5,15 @@ require 'carrierwave/storage/file'
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
-  # else Rails.env.production?
-  #   config.storage = :fog
-  #   config.fog_provider = 'fog/aws'
-  #   config.fog_credentials = {
-  #     provider: 'AWS',
-  #     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-  #     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-  #     region: 'ap-northeast-1' #例 'ap-northeas-1'
-  # }
+  else Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1' #例 'ap-northeas-1'
+  }
   end
   config.fog_directory  = 'free1209'
   # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/free1209'
